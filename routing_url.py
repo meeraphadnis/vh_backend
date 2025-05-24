178100421b19b3a9e3d001015ce775cc6ab26787
sanaabebal <sanaabebal@gmail.com>
2025-05-23 23:40:16 -0700

    rough sketch of routing_url.py

---
+++ routing_url.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter
+from validating_input import FinancialData
+from logic import generate_snowball_plan, generate_avalanche_plan
+
+router = APIRouter()
+
+@router.post("/repayment-plan")
+def repayment(data: FinancialData):
+    snowball = generate_snowball_plan(data)
+    avalanche = generate_avalanche_plan(data)
+    return {
+        "snowball": snowball,
+        "avalanche": avalanche
+    }
