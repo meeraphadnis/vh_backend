0c66c216f955c3b6e18097be81fa42ba6fa5c2e3
sanaabebal <sanaabebal@gmail.com>
2025-05-25 04:54:47 -0700

    fixed logic for find_financial_aid_cost

--- test.py
+++ test.py
@@ -11,13 +11,18 @@ client = genai.Client(api_key=gemini_api_key_variable)
 #     extracted_result = file.read()
 
 
-def find_financial_aid_cost(text)
+def find_financial_aid_cost(text):
+    finances_text_path = text
+    with open(finances_text_path, "r", encoding="utf-8") as file:
+        extracted_result = file.read()
     response = client.models.generate_content(
         model="gemini-2.0-flash",
         contents= f"Find the total cost of financial aid given from this file: {extracted_result}"
     )
     return response.text
 
+
+print(find_financial_aid_cost("extracted_pdf_content.txt"))
 # print(response.text)
 # print(response_two.text)
 # print(response_three.text)
